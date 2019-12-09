3145d1fd8625e2ffadcf51ba80e905d2414e4940
Ashmit Mittal <ashmit.mittal111@gmail.com>
2019-12-09 10:58:13 +0530

    states

--- ReactApp/update-complex-state/src/App.js
+++ ReactApp/update-complex-state/src/App.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  const [instructors,setName] = useState(
+  const [instructors,setValue] = useState(
     [
       {
         name:'Ashi',
@@ -17,17 +17,45 @@ function App() {
 
 
   const instructor = instructors.map((inst,index) => (
-    <li key={index}>
+    <li style={{color: "red"}} key={index}>
       <h3>{inst.name}</h3>
       <h4>hobbies: {inst.hobbies.join(", ")}</h4>
     </li>
   ));
+
+  // const remove = () => {
+  //   const NewInst = [...instructors];
+  //   const randomInst = Math.floor(
+  //     Math.random() * NewInst.length
+  //   );
+  //   const randomHobbies = Math.floor(
+  //     Math.random() * NewInst[randomInst].hobbies.length
+  //   );
+  //   NewInst[randomInst].hobbies.splice(randomHobbies,1);
+  //   setValue(NewInst);
+  // }
+
+  setTimeout(() => {
+    const NewInst = [...instructors];
+    const randomInst = Math.floor(
+      Math.random() * NewInst.length
+    );
+    const randomHobbies = Math.floor(
+      Math.random() * NewInst[randomInst].hobbies.length
+    );
+    NewInst[randomInst].hobbies.splice(randomHobbies,1);
+    setValue(NewInst);
+  }, 5000);
+
 
   return (
     <div className="App">
       <ul>
         {instructor}
       </ul>
+      <div className="foot">
+        <footer><h1><i>This code will automatically delete the hobbies randomlly after 5 sec an example of states with functional based component</i></h1></footer>
+      </div>
     </div>
   );
 }
