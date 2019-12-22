e7fe88b31c5e8e0717a5156d53e236bf194b0a3a
Ashmit Mittal <ashmit.mittal111@gmail.com>
2019-12-22 18:10:47 +0530

    Material UI

---
+++ ReactApp/material-ui/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Btn from "./Components/1";
+
+function App() {
+	return (
+		<div className="App">
+			{/* <h1>Material Ui Demo</h1> */}
+			<Btn />
+		</div>
+	);
+}
+
+export default App;
---
+++ ReactApp/material-ui/src/Components/1.js
@@ -0,0 +1,138 @@
+import React, { useState } from "react";
+import Drawer from "@material-ui/core/Drawer";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import Divider from "@material-ui/core/Divider";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import MenuIcon from "@material-ui/icons/Menu";
+import HomeRoundedIcon from "@material-ui/icons/HomeRounded";
+import InfoRoundedIcon from "@material-ui/icons/InfoRounded";
+import MessageRoundedIcon from "@material-ui/icons/MessageRounded";
+import BusinessIcon from "@material-ui/icons/Business";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		width: "100%",
+		flexGrow: 1
+	},
+	menuButton: {
+		marginRight: theme.spacing(0)
+	},
+	title: {
+		flexGrow: 1
+	},
+	list: {
+		width: 250
+	},
+	fullList: {
+		width: "auto"
+	}
+}));
+
+function Btn() {
+	const classes = useStyles();
+	const [state, setState] = useState({
+		left: false
+	});
+	const toggleDrawer = (side, open) => (event) => {
+		if (
+			event.type === "keydown" &&
+			(event.key === "Tab" || event.key === "Shift")
+		) {
+			return;
+		}
+
+		setState({ ...state, [side]: open });
+	};
+	const sideList = (side) => (
+		<div
+			className={classes.list}
+			role="presentation"
+			onClick={toggleDrawer(side, false)}
+			onKeyDown={toggleDrawer(side, false)}
+		>
+			<List>
+				<ListItem button>
+					<ListItemIcon>
+						<HomeRoundedIcon />
+					</ListItemIcon>
+					<ListItemText primary="Home"></ListItemText>
+				</ListItem>
+				<ListItem button>
+					<ListItemIcon>
+						<InfoRoundedIcon />
+					</ListItemIcon>
+					<ListItemText primary="About"></ListItemText>
+				</ListItem>
+				<ListItem button>
+					<ListItemIcon>
+						<MessageRoundedIcon />
+					</ListItemIcon>
+					<ListItemText primary="Blog"></ListItemText>
+				</ListItem>
+				<ListItem button>
+					<ListItemIcon>
+						<BusinessIcon />
+					</ListItemIcon>
+					<ListItemText primary="Partner with us"></ListItemText>
+				</ListItem>
+			</List>
+			<Divider />
+			<List>
+				{["User", "Logout"].map((text, index) => (
+					<ListItem button key={text}>
+						<ListItemIcon>
+							{index % 2 === 0 ? <AccountCircleIcon /> : <ExitToAppIcon />}
+						</ListItemIcon>
+						<ListItemText primary={text} />
+					</ListItem>
+				))}
+			</List>
+		</div>
+	);
+
+	return (
+		<div className={classes.root}>
+			<AppBar color="primary" position="static">
+				<Toolbar>
+					<IconButton edge="start" className={classes.menuButton}>
+						<MenuIcon onClick={toggleDrawer("left", true)} />
+					</IconButton>
+					<Typography variant="h6" className={classes.title}>
+						QuickStay
+					</Typography>
+					{/* <Button color="inhereted">Login</Button> */}
+				</Toolbar>
+			</AppBar>
+			<Drawer open={state.left} onClose={toggleDrawer("left", false)}>
+				{sideList("left")}
+			</Drawer>
+			<Typography variant="h3" gutterBottom>
+				QuickStay Rooms
+			</Typography>
+			<CssBaseline />
+			<Container maxWidth="lg">
+				<Typography
+					component="div"
+					style={{ backgroundColor: "#cfe8fc", height: "50vh" }}
+				>
+					<Typography variant="h4">LET US FIND YOU A COZY HOME</Typography>
+				</Typography>
+			</Container>
+		</div>
+	);
+}
+
+export default Btn;
