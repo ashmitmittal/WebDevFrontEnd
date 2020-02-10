21e147c6be3a1c9426bd9e63a023bc92123a0f7c
Ashmit Mittal <ashmit.mittal111@gmail.com>
2020-02-10 13:01:31 +0530

    scroll animation

--- GSAP_ScrollMagic/app.js
+++ GSAP_ScrollMagic/app.js
@@ -1,8 +1,21 @@
 var tl = new TimelineMax({ onUpdate: updatePercentage });
+var tl2 = new TimelineMax();
 
 const controller = new ScrollMagic.Controller();
 
 tl.from("blockquote", 0.5, { x: 200, opacity: 0 });
+tl.from("span", 1, { width: 0 }, "=-.5");
+tl.from("#coding", 1, { x: -200, opacity: 0 }, "=-1");
+tl.from("#google", 1, { x: 200, opacity: 0 }, "=-.7");
+
+tl2.from("#box", 1, { opacity: 0, scale: 0 });
+tl2.to("#box", 0.5, {
+	left: "20%",
+	scale: 1.3,
+	borderColor: "white",
+	borderWidth: 12,
+	boxShadow: "1px 1px 0px 0px rgba(0,0,0,.09)"
+});
 
 const scene = new ScrollMagic.Scene({
 	triggerElement: ".sticky",
@@ -11,6 +24,12 @@ const scene = new ScrollMagic.Scene({
 })
 	.setPin(".sticky")
 	.setTween(tl)
+	.addTo(controller);
+
+const scene2 = new ScrollMagic.Scene({
+	triggerElement: "blockquote"
+})
+	.setTween(tl2)
 	.addTo(controller);
 
 function updatePercentage() {
